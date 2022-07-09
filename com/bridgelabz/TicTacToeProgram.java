338701da339589e8b2c416a81a6df6e4d05fcdbf
MANOJ-51 <m4anoj51@gmail.com>
2022-07-09 09:08:53 +0530

    added: method to enter th user input

--- com/bridgelabz/TicTacToeProgram.java
+++ com/bridgelabz/TicTacToeProgram.java
@@ -1,18 +1,37 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class TicTacToeProgram {
 
     //creating board
-    public static void creatingBoard() {
+    public static char[] creatingBoard() {
         char[] board = new char[10];
         //zero is ignores for user friendly
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
+        return board;
+    }
+
+    //using letter x,0 to input
+    public static char userSymbol() {
+        char computerInput = 'O';
+        Scanner scanner = new Scanner(System.in);
+        char userInput = scanner.next().charAt(0);
+        if (userInput == computerInput) {
+            System.out.println("Player choose computer input ");
+        } else if (userInput != computerInput) {
+            computerInput = 'O';
+            System.out.println("player chosed" + computerInput);
+        }
+        return userInput;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome Tic Tac Toe program ");
         creatingBoard();
+        userSymbol();
     }
 }
+
