226534357097fad431b36489f622ca367475d6c9
MANOJ-51 <m4anoj51@gmail.com>
2022-07-09 08:43:03 +0530

    added: method for creating board

--- com/bridgelabz/TicTacToeProgram.java
+++ com/bridgelabz/TicTacToeProgram.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
 public class TicTacToeProgram {
+
+    //creating board
+    public static void creatingBoard() {
+        char[] board = new char[10];
+        //zero is ignores for user friendly
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome Tic Tac Toe program ");
+        creatingBoard();
     }
 }
